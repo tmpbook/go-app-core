b0615875d3d7483293b0aa43053c4bdcdcd70c53
临书 <tmpbook@users.noreply.github.com>
2017-09-22 17:05:40 +0800

    Add a http error decorator (#1)
    * add error decorator
    
    * remove the binaries

--- demo/controllers/ReloadConfigCtl.go
+++ demo/controllers/ReloadConfigCtl.go
@@ -2,25 +2,29 @@ package controllers
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/tmpbook/go-app-core/utils/common"
 )
 
 // ReloadConfigController R.T.
-type ReloadConfigController struct{}
-
-func (*ReloadConfigController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.Method)
-	if r.Method != "GET" {
-		http.Error(w, "Method Not Allowed", 405)
-		return
+func ReloadConfigController(w http.ResponseWriter, r *http.Request) error {
+	content, err := common.GetConfigByKey("content.say-hello")
+	if err != nil {
+		return fmt.Errorf("when get config by key: %v", err)
 	}
-	content, _ := common.GetConfigByKey("content.say-hello")
 	fmt.Fprintln(w, "old:", content.(string))
 
-	_ = common.ReloadConfig()
-	content, _ = common.GetConfigByKey("content.say-hello")
+	err = common.ReloadConfig()
+	if err != nil {
+		return fmt.Errorf("when reload config: %v", err)
+	}
+
+	content, err = common.GetConfigByKey("content.say-hello")
+	if err != nil {
+		return fmt.Errorf("when get config by key: %v", err)
+	}
 	fmt.Fprintln(w, "new:", content.(string))
+
+	return nil
 }
